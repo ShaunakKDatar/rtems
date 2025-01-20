ea8f882183d7227881d9fe86a902247304573ec7
ShaunakKDatar <shaunakkdatar@gmail.com>
2025-01-20 12:10:30 +0530

    bsp/aarch64/raspberrypi: Added the 10-bit slave addressing case

--- bsps/aarch64/raspberrypi/i2c/raspberrypi-i2c.c
+++ bsps/aarch64/raspberrypi/i2c/raspberrypi-i2c.c
@@ -1,3 +1,39 @@
+
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup RTEMSBSPsAArch64Raspberrypi4
+ *
+ * @brief I2C Driver
+ */
+
+/*
+ * Copyright (C) 2025 Shaunak Datar
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <bsp/raspberrypi-i2c.h>
 #include <bsp/raspberrypi.h>
 #include <bsp/rpi-gpio.h>
@@ -100,6 +136,13 @@ static int rpi_i2c_transfer(i2c_bus *base, i2c_msg *msgs, uint32_t msg_count)
         bus->current_buffer_size = msgs[i].len;
         bus->remaining_transfers = (bus->current_buffer_size + 0xFFFFE) / 0xFFFFF;
 
+        if (msgs[i].flags & I2C_M_TEN) // 10-bit slave address
+        {
+            /* Add the 8 lsbs of the 10-bit slave address to the fifo register*/
+            BCM2835_REG(bus->base_address + BCM2711_I2C_FIFO) = msgs[i].addr & 0xFF;
+            uint8_t msbs = msgs[i].addr >> 8;
+            BCM2835_REG(bus->base_address + BCM2711_I2C_SLAVE_ADDRESS) = (0x1E << 2) | msbs;
+        }
         BCM2835_REG(bus->base_address + BCM2711_I2C_SLAVE_ADDRESS) = msgs[i].addr;
 
         if (msgs[i].flags & I2C_M_RD)
